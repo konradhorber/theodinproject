213293635b5be3a41257afca56b9917d50b80db9
Konrad Horber <konrad.horber@gmail.com>
2024-02-28 08:17:04 +0100

    Experiment with javascript
    Perform random experimentation with javascript
    and tried out a lot of stuff

---
+++ javascript/javascript.js
@@ -0,0 +1,17 @@
+function add7(number) {
+  return number + 7;
+}
+
+function multiply(number_one, number_two) {
+  return number_one * number_two;
+}
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function lastLetter(string) {
+  return string.at(-1);
+}
+
+console.log(lastLetter('konrad'))
