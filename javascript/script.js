a63d56e82ff7125c561d540010fb8ac035629d2e
konradhorber <konrad.horber@gmail.com>
2024-09-09 11:39:04 +0200

    Update notepads for exercises object basics

--- javascript/script.js
+++ javascript/script.js
@@ -1,59 +1,41 @@
-const container = document.querySelector("#container");
+let user = {
+    name: "John",
+    surname: "Smith",
+};
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
+user.name = "Pete";
 
-container.appendChild(content);
+delete user.name;
 
-// 1. a <p> with red text that says “Hey I’m red!”
-const redText = document.createElement("p");
+let schedule = {};
 
-redText.classList.add("redText");
-redText.style.color = "red";
-redText.textContent = "Hey I'm red!";
+function isEmpty(obj) {
+    for(let key in obj) {
+        return false;
+    };
+    return true;
+}
 
-container.appendChild(redText);
+let salaries = {
+  }
 
-// 2. an <h3> with blue text that says “I’m a blue h3!”
+let sum = 0;
+for(let key in salaries) {
+    sum += salaries[key];
+}
 
-const blueHThree = document.createElement("h3");
+let menu = {
+    width: 200,
+    height: 300,
+    title: "my menu",
+};
 
-blueHThree.classList.add("blueHThree");
-blueHThree.style.color = "blue";
-blueHThree.textContent = "I'm a blue h3!";
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if(Number.isInteger(obj[key])) {
+            obj[key] = obj[key] * 2;
+        }
+    }
+}
 
-container.appendChild(blueHThree)
-
-// 3. a <div> with a black border and pink background color with the following 
-// elements inside of it:
-const blackPinkDiv = document.createElement("div");
-
-blackPinkDiv.setAttribute(
-    "style",
-    "background: pink; border: 1px solid black;"
-);
-
-container.appendChild(blackPinkDiv);
-
-// another <h1> that says “I’m in a div”
-const divHOne = document.createElement("h1");
-
-divHOne.textContent = "I'm in a div";
-
-blackPinkDiv.appendChild(divHOne);
-
-// a <p> that says “ME TOO!”
-const pMeToo = document.createElement("p");
-
-pMeToo.textContent = "ME TOO!";
-
-blackPinkDiv.appendChild(pMeToo);
-
-
-// events part
-const btn = document.querySelector("#btn");
-
-btn.addEventListener("click", function (e) {
-    e.target.style.background = "blue";
-  });
+multiplyNumeric(menu);
